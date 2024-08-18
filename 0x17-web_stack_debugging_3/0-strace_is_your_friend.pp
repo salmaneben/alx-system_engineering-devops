59f7201bef220958bd6eeadfc1c2092421f8b8b6
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-08-18 09:17:29 -0700

    a

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,8 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# a puppet code that fixes a wordpress site 5xx error to 200 ok
+# by editing the mistyped .php to php in the /var/www/html/wp-settings.php file
 
-exec { 'fix-wordpress':
+exec { 'fix-wordpress-server-error':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/bin/:/bin/',
 }
+
