846576e051600eee7c62aa01c89e5f63ca8b7bad
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-08-25 05:38:13 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""Module for task 1"""
 
+import requests
 
-def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        return 0
-
-    return sub_info.json().get("data").get("subscribers")
+def top_ten(subreddit):
+    """Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit"""
+    
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code >= 300:
+        print(None)
+        return
+    
+    data = response.json().get("data")
+    
+    if data is None:
+        print(None)
+        return
+    
+    posts = data.get("children")
+    
+    if not posts:
+        print(None)
+        return
+    
+    for post in posts:
+        print(post.get("data").get("title"))
