35ec2bb3502cbd0c2ebf8a6035d99ef6e3ebb46b
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-08-25 06:40:19 -0700

    e

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -4,9 +4,27 @@
 """
 import sys
 
+def number_of_subscribers(subreddit):
+    import requests
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    data = response.json().get("data")
+    num_subs = data.get("subscribers", 0)  # Default to 0 if not found
+
+    return num_subs
+
 if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        subscribers = number_of_subscribers(sys.argv[1])
+        if subscribers > 0:
+            print("OK")
+        else:
+            print("0")
+
