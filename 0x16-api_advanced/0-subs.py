39593e76cf796f338f7f23a69108ea1e9c93d187
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-08-25 05:40:45 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+"""Module for task 0"""
 
 import requests
 
-def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit"""
-    
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "My-User-Agent"}
     
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code >= 300:
-        print(None)
-        return
-    
-    data = response.json().get("data")
-    
-    if data is None:
-        print(None)
-        return
-    
-    posts = data.get("children")
-    
-    if not posts:
-        print(None)
-        return
-    
-    for post in posts:
-        print(post.get("data").get("title"))
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        data = response.json().get("data", {})
+        return data.get("subscribers", 0)
+    except requests.RequestException:
+        return 0
